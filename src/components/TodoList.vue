8d1b3edb5add7edfcc2df5d1e5f81e682051911a
Sergey Popov <s.popov@ors.aero>
2023-12-07 16:28:51 +0300

    init repo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
